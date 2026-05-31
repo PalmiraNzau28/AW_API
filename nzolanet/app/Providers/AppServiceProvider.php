80a81589818b1d298394d4508214f671617be3c1
josue.dosidiana <106892987+Koder012@users.noreply.github.com>
2026-05-31 14:37:01 +0100

    update:criado o controller para os comentarios

--- nzolanet/app/Providers/AppServiceProvider.php
+++ nzolanet/app/Providers/AppServiceProvider.php
@@ -2,28 +2,17 @@
 
 namespace App\Providers;
 
-<<<<<<< HEAD
-=======
+
 use App\Repositories\AuthRepository;
 use App\Repositories\Interfaces\AuthRepositoryInterface;
->>>>>>> 2a85dbadae3410ad595aaac69ed606fa6335e014
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-<<<<<<< HEAD
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      */
-=======
+
     public function register(): void
     {
         $this->app->bind(
@@ -32,7 +21,6 @@ class AppServiceProvider extends ServiceProvider
         );
     }
 
->>>>>>> 2a85dbadae3410ad595aaac69ed606fa6335e014
     public function boot(): void
     {
         //
